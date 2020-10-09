0642e917012a07c946aaad8427de00a1a17e3a0f
Camila Campos <camilacamposramos.si@gmail.com>
2020-10-09 18:32:51 -0300

    cap26: exercise 2 - add gif

--- cap26/exercise1/cachorro/dog.go
+++ cap26/exercise1/cachorro/dog.go
@@ -4,7 +4,7 @@ package cachorro
 // - Coloque seu código no GitHub.
 // - Faça sua documentação aparecer em godoc.org, e tire um screenshot. (doc_on_godoc.png)
 // - Delete seu código do GitHub.
-// - Faça um refresh em godoc.org e veja se seu código sumiu.
+// - Faça um refresh em godoc.org e veja se seu código sumiu. (refreshing_after_moving.mov)
 
 // Idade recebe anos (inteiro) e retorna a idade equivalente em anos de cachorro
 // 		1 ano humano -> 7 anos caninos
