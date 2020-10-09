ea6ff2e7ee79da9bde018d4b11a92c7e480a5a5b
Camila Campos <camilacamposramos.si@gmail.com>
2020-10-09 18:30:15 -0300

    cap26: exercise 2 - moving package around

--- cap26/exercise1/cachorro/dog.go
+++ cap26/exercise1/cachorro/dog.go
@@ -1,6 +1,11 @@
 // Pacotoe cachorro define como se calcular idades de cachorros a partir de anos humanos
 package cachorro
 
+// - Coloque seu código no GitHub.
+// - Faça sua documentação aparecer em godoc.org, e tire um screenshot. (doc_on_godoc.png)
+// - Delete seu código do GitHub.
+// - Faça um refresh em godoc.org e veja se seu código sumiu.
+
 // Idade recebe anos (inteiro) e retorna a idade equivalente em anos de cachorro
 // 		1 ano humano -> 7 anos caninos
 func Idade(anos int) int {
